119221b4238cca1da018bdc5ac515cb8cdb72b40
bittenhelen1983 <bittenhelen@gmail.com>
2022-10-25 14:20:32 +0200

    week 7
    First push

---
+++ js/favorites.js
@@ -0,0 +1,33 @@
+import { getExistingFavs } from "./favFunctions.js";
+
+const favourites = getExistingFavs();
+
+const recipeContainer = document.querySelector(".recipeContainer");
+
+if(favourites.lenght === 0) {
+recipeContainer.innerHTML = "No favourites yet..."
+}
+
+favourites.forEach(favourite => {
+    recipeContainer.innerHTML += `<div class="recipe">
+                                    <div class="thumbnailContainer"><img class="thumbnail" src="${favourite.image}"/></div>
+                                    <h2>${favourite.title}</h2>
+                                    <i class="fa fa-heart"></i> 
+                                    </div>`
+    
+});
+
+
+async function thumbnailSize() {
+    
+    let thumbnail = document.querySelector(".thumbnail")
+
+    if(thumbnail.style.height >= thumbnail.style.width) {
+
+        thumbnail.style.height = `200px`
+    }   else {
+        thumbnail.style.width = `200px`
+}
+}
+
+thumbnailSize();
---
+++ js/results.js
@@ -0,0 +1,104 @@
+const recipeContainer = document.querySelector(".recipeContainer");
+
+import { getExistingFavs } from "./favFunctions.js";
+
+const api = {
+	method: 'GET',
+	headers: {
+		'X-API-Key': ' 8d13f27b766444338fcdf22ed323afc8'
+	}
+};
+
+const url = "https://api.spoonacular.com/recipes/complexSearch??query=healthy&type=main%20course&instructionsRequired=true&addRecipeInformation=true&sortDirection=asc&offset=0&number=100"
+
+const corsFix = "https://noroffcors.herokuapp.com/" + url;
+
+const favourites = getExistingFavs();
+
+async function getRecipe() {
+
+    const response = await fetch(corsFix, api, favourites);
+
+    const json = await response.json();
+
+    const recipe = json.results;
+
+    console.log(recipe);
+
+    recipe.forEach(recipe => {
+        let cssClass = "far";
+
+        const doesObjectExist = favourites.find(function(fav) {
+            console.log(fav);
+            return parseInt(fav.id) === recipe.id;
+        })
+
+        if(doesObjectExist) {
+            cssClass = "fa";
+        }
+        else
+        recipeContainer.innerHTML += `<div class="recipe">
+                                        <div class="thumbnailContainer"><img class="thumbnail" src="${recipe.image}"/> </div>
+                                       <h2>${recipe.title}</h2>
+                                        <i class="${cssClass} fa-heart" data-id="${recipe.id}" data-title="${recipe.title}"></i> 
+                                        </div>`
+        
+    });
+
+    const favButtons = document.querySelectorAll(".recipe i");
+
+
+
+favButtons.forEach(heart => {
+    heart.addEventListener("click", handleClick);
+});
+
+function handleClick() {
+    
+    this.classList.toggle("fa");
+    this.classList.toggle("far");
+
+    const id = this.dataset.id;
+    
+    const title = this.dataset.title;
+
+    const currentFavs = getExistingFavs();
+
+    const recipeExists = currentFavs.find(function(fav) {
+        return fav.id === id;
+    });
+
+    console.log("recipeExist", recipeExists);
+
+    if(!recipeExists) {
+        const recipe = {id: id, title: title};
+        currentFavs.push(recipe);
+        saveFavs(currentFavs);
+    }
+    else {
+        const newFavs = currentFavs.filter(fav => fav.id !== id);
+        saveFavs(newFavs);
+    }
+}
+
+function saveFavs(favs) {
+    localStorage.setItem("favourites", JSON.stringify(favs));
+}
+}
+
+getRecipe();
+
+
+async function thumbnailSize() {
+    
+    let thumbnail = document.querySelector(".thumbnail")
+
+    if(thumbnail.style.height >= thumbnail.style.width) {
+
+        thumbnail.style.height = `200px`
+    }   else {
+        thumbnail.style.width = `200px`
+}
+}
+
+thumbnailSize();
