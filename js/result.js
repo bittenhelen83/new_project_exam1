19f44d772889a6eba4c6bdb2bdcd48952659724a
bittenhelen1983 <bittenhelen@gmail.com>
2022-10-29 13:19:53 +0200

    Update result.js

--- js/result.js
+++ js/result.js
@@ -1,5 +1,5 @@
 import { displayMessage } from "./utils/displayMessage.js";
-import { options, url } from "./constants/optionsResults.js";
+import { options, corsFix} from "./constants/optionsResults.js";
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
@@ -8,7 +8,7 @@ const worksId = params.get("id");
 console.log(worksId); 
 
 async function getRecipes() {
-    const response = await fetch(options, url);
+    const response = await fetch(options, corsFix);
     const results = await response.json();
     console.log(results);
 }
