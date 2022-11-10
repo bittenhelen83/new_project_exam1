32720355a9ac92cae8233dcbddb360776c7d52ba
bittenhelen1983 <bittenhelen@gmail.com>
2022-11-10 17:16:27 +0100

    End of points

--- js/result.js
+++ js/result.js
@@ -7,27 +7,50 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-console.log(id);
-
 async function getSingleResult() {
-    try    {
-        const response = await fetch(`https://noroffcors.herokuapp.com/https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=8d13f27b766444338fcdf22ed323afc8`);
+    try {
+        const response = await fetch(`https://noroffcors.herokuapp.com/https://api.spoonacular.com/recipes/${id}/information?apiKey=8d13f27b766444338fcdf22ed323afc8`)
         const singleResult = await response.json();
-      
-        console.log(singleResult);
         
+        console.log(singleResult);
+
         resultContainer.innerHTML = "";
 
-        singleResult.forEach(singleResult => {
+        singleResult.forEach(singleRes => {
+            resultContainer.innerHTML += `<div class="singleResult">
+                                            <h2>${singleRes.title}</h2>
+                                        </div>`
+            
+            console.log(singleRes.title);
 
-            resultContainer.innerhtml += `<div>
-                                    <p class="steps">${singleResult.steps[0].step}</p>
-                                    </div>`
-            console.log(singleResult.steps[0].step);
-        });                         
+        });
     } catch {
         // displayMessage();
     }
+
 }
 
 getSingleResult();
+
+// async function getSingleResult() {
+//     try    {
+//         const response = await fetch(`https://noroffcors.herokuapp.com/https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=8d13f27b766444338fcdf22ed323afc8`);
+//         const singleResult = await response.json();
+      
+//         console.log(singleResult);
+        
+//         resultContainer.innerHTML = "";
+
+//         singleResult.forEach(singleResult => {
+
+//             resultContainer.innerHTML += `<div class="singleResult">
+//                                             ${singleResult.steps.map(step => `<p class="steps">&#x2022; ${step.step}</p>`).join("")}
+//                                         </div>`
+//             console.log(singleResult.steps[0].step);
+//         });                         
+//     } catch {
+//         // displayMessage();
+//     }
+// }
+
+// getSingleResult();
